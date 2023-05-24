737978c013dd5f58f6a5c05075aa9a3ee2fe877c
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-05-24 19:57:34 +0545

    Nav menu registration

--- functions.php
+++ functions.php
@@ -11,13 +11,36 @@
 
 function uv_woo_enqueue_styles_and_scripts(): void
 {
-    // enqueue script
+    // Enqueue script
     wp_enqueue_script('bootstrap-script', get_template_directory_uri() . '/inc/bootstrap.min.js', ['jquery'], '5.3.0', true);
 
-    // enqueue style
+    // Enqueue style
     wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/inc/bootstrap.min.css', [], '5.3.0', 'all');
-    wp_enqueue_style('uv-woo-main-style', get_stylesheet_uri(), [], filemtime(get_template_directory().'/style.css'), 'all');
+    wp_enqueue_style('uv-woo-main-style', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
 
 }
 
 add_action('wp_enqueue_scripts', 'uv_woo_enqueue_styles_and_scripts');
+
+/**
+ * Disable Gutenberg Editor
+ *
+ * Disables the Gutenberg editor and reverts to the classic editor for all post types.
+ */
+
+function uv_woo_disable_gutenberg_editor(): void
+{
+    add_filter('use_block_editor_for_post_type', '__return_false', 10); // Disable Gutenberg editor for all post types
+}
+add_action('init', 'uv_woo_disable_gutenberg_editor');
+
+function uv_woo_config()
+{
+register_nav_menus(
+    [
+        'uv_woo_main_menu'=>'Uv Woo Nav Menu'
+    ]
+);
+}
+
+add_action('after_setup_theme', 'uv_woo_config', 0);
