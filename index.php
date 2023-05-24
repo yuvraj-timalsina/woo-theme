d11bc52087bbcf9570556ed72077b6fe39a82cc5
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-05-24 12:34:08 +0545

    Design skeleton

--- index.php
+++ index.php
@@ -6,8 +6,111 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Uv Woo</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/css/bootstrap.min.css">
 </head>
-<body>
-<?= "Hello World!" ?>
+<body <?php body_class() ?>>
+<?php wp_body_open() ?>
+<div id="page" class="site">
+    <!-- Header -->
+    <header class="bg-light">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Logo -->
+                logo
+                <div class="col-6 col-md-3">
+                    <!-- Logo code goes here -->
+                </div>
+
+                <!-- Account -->
+                <div class="col-6 col-md-3 text-end">
+                    <!-- Account code goes here -->
+                    account
+                </div>
+
+                <!-- Search -->
+                <div class="col-md-6 mt-3 mt-md-0">
+                    <!-- Search form code goes here -->
+                    search
+                </div>
+
+                <!-- Menu -->
+                <nav class="col-12 mt-3 mt-md-0">
+                    <!-- Menu code goes here -->
+                    menu
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <!-- Slider -->
+    <div class="slider">
+        <div class="container">
+            <!-- Slider code goes here -->
+            slider
+        </div>
+    </div>
+
+    <!-- Promotions -->
+    <div class="promotions">
+        <div class="container">
+            <!-- Promotions code goes here -->
+            promotions
+        </div>
+    </div>
+
+    <!-- Popular Products -->
+    <div class="popular-products">
+        <div class="container">
+            <!-- Popular products code goes here -->
+            popular
+        </div>
+    </div>
+
+    <!-- New Arrivals -->
+    <div class="new-arrivals">
+        <div class="container">
+            <!-- New arrivals code goes here -->
+            new arrivals
+        </div>
+    </div>
+
+    <!-- Deal of the Week -->
+    <div class="deal-of-the-week">
+        <div class="container">
+            <!-- Deal of the week code goes here -->
+            deal of the week
+        </div>
+    </div>
+
+    <!-- News -->
+    <div class="news">
+        <div class="container">
+            <!-- News code goes here -->
+            news
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="bg-dark text-light">
+        <div class="container">
+            <div class="row">
+                <!-- Footer Widgets -->
+                <div class="col-md-6 col-lg-8">
+                    <!-- Footer widgets code goes here -->
+                    footer widgets
+                </div>
+
+                <!-- Copyright -->
+                <div class="col-md-6 col-lg-4 text-center text-md-end">
+                    <!-- Copyright code goes here -->
+                    copyright
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/js/bootstrap.bundle.min.js"></script>
+
+</div>
 </body>
 </html>
