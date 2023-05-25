6e195b767dcb90aa110298b003f8ffdf443fbc73
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-05-25 22:24:48 +0545

    css addition

--- page.php
+++ page.php
@@ -20,7 +20,7 @@ get_header();
                         if (have_posts()) :
                             while (have_posts()) :
                                 the_post(); ?>
-                                <article>
+                                <article class="col">
                                     <h1><?php the_title(); ?> </h1>
                                     <div><?php the_content(); ?></div>
                                 </article>
