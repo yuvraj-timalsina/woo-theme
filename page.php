fcb023af8178758f09c2f59d28eb1b91fd90b72f
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-05-30 09:31:18 +0545

    single post

--- page.php
+++ page.php
@@ -14,24 +14,17 @@ get_header();
 ?>
     <div class="content-area">
         <main>
-                <div class="container">
-                    <div class="row">
-                        <?php
-                        if (have_posts()) :
-                            while (have_posts()) :
-                                the_post(); ?>
-                                <article class="col">
-                                    <h1><?php the_title(); ?> </h1>
-                                    <div><?php the_content(); ?></div>
-                                </article>
-                            <?php
-                            endwhile;
-                        else:
-                            ?>
-                            <p>No Posts!</p>
-                        <?php endif; ?>
-                    </div>
+            <div class="container">
+                <div class="row">
+					<?php
+					while ( have_posts() ) : the_post(); ?>
+                        <article class="col">
+                            <h1><?php the_title(); ?> </h1>
+                            <div><?php the_content(); ?></div>
+                        </article>
+					<?php endwhile; ?>
                 </div>
+            </div>
         </main>
     </div>
 
---
+++ single.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ * @package Uv Woo
+ */
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+            <div class="container">
+                <div class="row">
+					<?php
+					while ( have_posts() ) :the_post(); ?>
+                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                            <header>
+                                <h1><?php the_title(); ?></h1>
+                                <div class="meta">
+                                    <p>Author : <?php the_author_posts_link(); ?> | <?= get_the_date(); ?>
+                                        <br>
+			                            <?php if ( has_category() ) : ?>
+                                            Categories : <span><?php the_category( ' ' ); ?></span>
+			                            <?php endif; ?>
+                                        <br>
+			                            <?php if ( has_tag() ) : ?>
+                                            Tags : <span><?php the_tags( '', ', ' ); ?></span>
+			                            <?php endif; ?>
+                                    </p>
+                                </div>
+                                <div class="post-thumbnail">
+									<?php
+									if ( has_post_thumbnail() ) :
+										the_post_thumbnail( 'uv-woo-blog', [ 'class' => 'img-fluid' ] );
+									endif;
+									?>
+                                </div>
+                            </header>
+                            <div class="content">
+								<?php the_content(); ?>
+                            </div>
+                        </article>
+					<?php
+					endwhile;
+					?>
+                </div>
+            </div>
+
+        </main>
+    </div>
+
+<?php get_footer(); ?>
